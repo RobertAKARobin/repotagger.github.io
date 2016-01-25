1ba3e2e43be582cad5833dc7ff6bc844772899fd
RobertAKARobin <robertgfthomas@gmail.com>
2016-01-25 09:55:04 -0500

    Error messaging if cache load exceeded

--- js/ajax.factory.js
+++ js/ajax.factory.js
@@ -6,47 +6,57 @@
   .factory("AJAX", AJAXFactory);
 
   function AJAXFactory(){
-    return function AJAX(method, url, options, callback){
-      var http = new XMLHttpRequest(), ETag;
-      if(options.params) url += "?" + queryString(options.params);
-      console.log(url);
+    return AJAX;
+  }
+
+  function AJAX(method, url, options, callback){
+    var request = {};
+    if(options.params) url += "?" + queryString(options.params);
+    if(!options.headers) options.headers = {};
+
+    (function hasETag(){
+      var ETag = localStorage.getItem(url);
+      if(localStorage && ETag) options.headers["If-None-Match"] = '"' + ETag + '"';
+    }());
+
+    (function send(){
+      var http = request.http = new XMLHttpRequest();
       http.open((method || "get"), url);
       if(options.headers) Object.keys(options.headers).forEach(function(header){
         http.setRequestHeader(header, options.headers[header]);
       });
-      if(localStorage && (ETag = localStorage.getItem(url))){
-        http.setRequestHeader("If-None-Match", '"' + ETag + '"');
-      }
       http.onreadystatechange = function(){
-        if(http.readyState === 4){
-          parseResponse(http, callback);
-        }
+        if(http.readyState === 4) parseResponse();
       }
       http.send();
-    }
+    }());
 
-    function parseResponse(http, callback){
-      var response = http.response;
-      var headers = parseHeaders(http);
-      var ETag = (headers.ETag || headers.Etag || headers.etag).replace(/[^a-z0-9]/g, "");
+    function parseResponse(){
+      var response = request.http.response;
+      var headers = parseHeaders();
+      var ETag = (headers.ETag || headers.Etag || headers.etag || "").replace(/[^a-z0-9]/g, "");
       var rateLimit = headers["X-RateLimit-Remaining"];
-      if(localStorage){
-        if(http.status === 304){
-          console.log("Retrieving from cache...");
+      if(localStorage && ETag){
+        if(request.http.status === 304){
           response = localStorage.getItem(ETag);
           headers = JSON.parse(localStorage.getItem(ETag + "-header"));
         }else{
-          localStorage.setItem(http.responseURL, ETag);
-          localStorage.setItem(ETag, response);
-          localStorage.setItem(ETag + "-header", JSON.stringify(headers));
-          if(rateLimit) console.log("Rate limit: " + rateLimit);
+          try{
+            localStorage.setItem(request.http.responseURL, ETag);
+            localStorage.setItem(ETag, response);
+            localStorage.setItem(ETag + "-header", JSON.stringify(headers));
+          }catch(e){
+            console.log("LocalStorage maxed out; clearing...");
+            localStorage.clear();
+          }
         }
       }
-      callback(JSON.parse(response), headers, http);
+      if(rateLimit) console.log("Rate limit remaining: " + rateLimit);
+      callback(JSON.parse(response), headers, request.http);
     }
 
-    function parseHeaders(http){
-      var output = {}, headerString = http.getAllResponseHeaders();
+    function parseHeaders(){
+      var output = {}, headerString = request.http.getAllResponseHeaders();
       headerString.split(/\n/).forEach(function(line){
         var split = -1, key = "", val = "";
         if((split = line.indexOf(":")) < 0) return;
