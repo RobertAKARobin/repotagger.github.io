32f5ef3e5493a4dbb68371e1ff7fe657157f8fa8
RobertAKARobin <robertgfthomas@gmail.com>
2016-01-31 18:42:08 -0500

    Extracted API to own factory

--- app.js
+++ app.js
@@ -4,15 +4,92 @@
   angular
   .module("repotagger", [ ])
   .config(["$locationProvider", AppConfig])
+  .factory("APIQuery", APIQuery)
   .controller("MainController", MainController);
 
   function AppConfig($locationProvider){
     $locationProvider.html5Mode(true);
   }
 
-  MainController.$inject = [ "$http", "$location", "$scope" ];
+  APIQuery.$inject = [ "$http" ];
+  function APIQuery($http){
+    var q;
+    var pvt = {};
+    return function(name){
+      q = this;
+      q.name = name;
+      pvt.tagCounts = {};
+      q.status = 100;
+      q.page = 0;
+      q.last = null;
+      q.repos = [];
+      q.tags = [];
+      q.untagged = 0;
+      q.page = 0;
+      send("https://api.github.com/users/" + q.name + "/repos", {
+        params:{ per_page: "100", access_token: "03b86161b45561bc7448eebac1c2a4491ebbf941"}
+      });
+    }
+    function send(url, options){
+      q.page += 1;
+      if(!options) options = {};
+      if(!options.headers) options.headers = {};
+      pvt.etag = localStorage.getItem(q.name + "," + q.page);
+      options.headers["If-None-Match"] = '"' + pvt.etag + '"';
+      $http.get(url, options).error(cached).then(parsed);
+    }
+    function cached(a,status,b,options){
+      if(status === 304){
+        console.log("Retrieving from cache...");
+        handle(JSON.parse(localStorage.getItem(pvt.etag)), JSON.parse(localStorage.getItem(pvt.etag + "-header")));
+      }else q.status = status;
+    }
+    function parsed(response){
+      var headers = response.headers();
+      var ETag = (headers.ETag || headers.Etag || headers.etag || "").replace(/[^a-z0-9]/g, "");
+      console.log("Rate limit remaining: " + headers["x-ratelimit-remaining"]);
+      try{
+        localStorage.setItem(q.name + "," + q.page, ETag);
+        localStorage.setItem(ETag, JSON.stringify(response.data));
+        localStorage.setItem(ETag + "-header", JSON.stringify(headers));
+      }catch(e){
+        console.log("Local storage maxed out! Clearing...");
+        localStorage.clear();
+      }
+      handle(response.data, headers);
+    }
 
-  function MainController($http, $location, $scope){
+    function handle(data, headers){
+      var links = {}, string = (headers.link || headers.Link);
+      if(string) string.split(",").forEach(function(line){
+        var pair = line.split(";");
+        var rel = pair[1].replace(/rel="(.*)"/, "$1").trim();
+        links[rel] = pair[0].replace(/<(.*)>/, "$1").trim();
+      });
+      if(links.last) q.last = "/" + links.last.substring(links.last.length - 1);
+      if(links.next) send(links.next);
+      else q.status = 200;
+      data.forEach(function(repo){
+        var tagMatcher = /\[[^\]]+\]/;
+        repo.description_sans_tags = (repo.description || "").replace(tagMatcher, "").trim();
+        if(repo.description && (tagMatcher.test(repo.description))){
+          repo.tags = repo.description.match(tagMatcher)[0].toLowerCase().replace(/[\[\]]/g, "").trim();
+          repo.tags = (repo.tags === "") ? [] : repo.tags.split(/, */);
+          repo.tags.forEach(function(tag){
+            pvt.tagCounts[tag] = (pvt.tagCounts[tag] || 0) + 1;
+          });
+        }else q.untagged += 1;
+      });
+      q.tags = [];
+      Object.keys(pvt.tagCounts).forEach(function(tag){
+        q.tags.push({name: tag, count: pvt.tagCounts[tag]});
+      });
+      q.repos = q.repos.concat(data);
+    }
+  }
+
+  MainController.$inject = [ "$location", "APIQuery" ];
+  function MainController($location, APIQuery){
     var vm = this;
     vm.status = 0;
     vm.name = $location.search().name;
@@ -20,15 +97,13 @@
     vm.tagSortAscend = true;
     vm.repoSort = "name";
     vm.repoSortAscend = false;
-    vm.startAPIQuery = startAPIQuery;
 
-    (function onLoad(){
-      if(!vm.name) vm.name = "repotagger";
-      startAPIQuery();
-    }());
-
+    vm.startAPIQuery = function(){
+      $location.search("name", vm.name.toLowerCase());
+      vm.data = new APIQuery(vm.name);
+    };
     vm.submitForm = function($event){
-      if($event.keyCode == 13) startAPIQuery();
+      if($event.keyCode == 13) vm.startAPIQuery();
     }
     vm.shouldFlash = function(){
       return (vm.name.toLowerCase() === "repotagger" ? "flash" : null);
@@ -44,91 +119,13 @@
       else if(!filter || filter === "all") return true;
       else return false;
     }
-    vm.repoSortBy = function(byWhat){
+    vm.repoSortBy = function(what){
       vm.repoSortAscend = !(vm.repoSortAscend);
-      vm.repoSort = byWhat;
+      vm.repoSort = what;
     }
-
-    function startAPIQuery(){
-      vm.status = 100;
-      vm.repos = [];
-      vm.tags = [];
-      vm.tagCounts = {};
-      vm.untagged = 0;
-      vm.page = 0;
-      vm.last = null;
-      $location.search("name", vm.name.toLowerCase());
-      new Query();
+    vm.init = function(){
+      if(!vm.name) vm.name = "repotagger";
+      vm.startAPIQuery();
     }
-
-    function Query(){
-      var q = this;
-      send("https://api.github.com/users/" + vm.name + "/repos", {
-        params:{ per_page: "100", access_token: "03b86161b45561bc7448eebac1c2a4491ebbf941"}
-      });
-
-      function send(url, options){
-        vm.page += 1;
-        if(!options) options = {};
-        if(!options.headers) options.headers = {};
-        q.etag = localStorage.getItem(vm.name + "," + vm.page);
-        options.headers["If-None-Match"] = '"' + q.etag + '"';
-        $http.get(url, options).error(cached).then(parsed);
-      }
-
-      function cached(a,status,b,options){
-        if(status === 304){
-          console.log("Retrieving from cache...");
-          handle(JSON.parse(localStorage.getItem(q.etag)), JSON.parse(localStorage.getItem(q.etag + "-header")));
-        }else{
-          vm.status = 403;
-        }
-      }
-
-      function parsed(response){
-        var headers = response.headers();
-        var ETag = (headers.ETag || headers.Etag || headers.etag || "").replace(/[^a-z0-9]/g, "");
-        console.log("Rate limit remaining: " + headers["x-ratelimit-remaining"]);
-        try{
-          localStorage.setItem(vm.name + "," + vm.page, ETag);
-          localStorage.setItem(ETag, JSON.stringify(response.data));
-          localStorage.setItem(ETag + "-header", JSON.stringify(headers));
-        }catch(e){
-          console.log("Local storage maxed out! Clearing...");
-          localStorage.clear();
-        }
-        handle(response.data, headers);
-      }
-
-      function handle(data, headers){
-        var links = {}, string = (headers.link || headers.Link);
-        if(string) string.split(",").forEach(function(line){
-          var pair = line.split(";");
-          var rel = pair[1].replace(/rel="(.*)"/, "$1").trim();
-          links[rel] = pair[0].replace(/<(.*)>/, "$1").trim();
-        });
-        if(links.last) vm.last = "/" + links.last.substring(links.last.length - 1);
-        if(links.next) send(links.next);
-        else vm.status = 200;
-        data.forEach(function(repo){
-          var tagMatcher = /\[[^\]]+\]/;
-          repo.description_sans_tags = (repo.description || "").replace(tagMatcher, "").trim();
-          if(repo.description && (tagMatcher.test(repo.description))){
-            repo.tags = repo.description.match(tagMatcher)[0].toLowerCase().replace(/[\[\]]/g, "").trim();
-            repo.tags = (repo.tags === "") ? [] : repo.tags.split(/, */);
-            repo.tags.forEach(function(tag){
-              vm.tagCounts[tag] = (vm.tagCounts[tag] || 0) + 1;
-            });
-          }else vm.untagged += 1;
-        });
-        vm.tags = [];
-        Object.keys(vm.tagCounts).forEach(function(tag){
-          vm.tags.push({name: tag, count: vm.tagCounts[tag]});
-        });
-        vm.repos = vm.repos.concat(data);
-      }
-
-    }
-
   }
 }());
